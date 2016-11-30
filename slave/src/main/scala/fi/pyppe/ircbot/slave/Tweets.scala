7547d1c2d84187d8dd69ba00f7b376ec254232dd
Pyry-Samuli Lahti <pyppe@iki.fi>
2016-11-30 12:48:01 +0200

    Remove urls in Twitter statuses

--- slave/src/main/scala/fi/pyppe/ircbot/slave/Tweets.scala
+++ slave/src/main/scala/fi/pyppe/ircbot/slave/Tweets.scala
@@ -8,8 +8,14 @@ object Tweets {
   def statusText(id: Long)(implicit ec: ExecutionContext): Future[String] = Future {
     val status = TwitterFactory.getSingleton.showStatus(id)
     val user = s"@${status.getUser.getScreenName} (${status.getUser.getName})"
-    val text = status.getText.replace("\n", " ")
+    val text = removeUrls(status.getText.replace("\n", " "))
+
     s"$user: $text"
   }
+
+  private def removeUrls(str: String) = str.replaceAll(
+    "https?://\\S+\\b",
+    ""
+  ).trim
 
 }
